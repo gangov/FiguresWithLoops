56d675e9e1693007a39b9d04695d8f516ea1df14
gangov <gangov1@gmail.com>
2018-03-19 01:45:25 +0200

    rombus working, but why?

--- src/rombusOfStars.java
+++ src/rombusOfStars.java
@@ -5,13 +5,43 @@ public class rombusOfStars {
         Scanner scan = new Scanner(System.in);
 
         int side = Integer.parseInt(scan.nextLine());
-        
-        for (int upperRow = 1; upperRow <= side ; upperRow++) {
-            for (int col = 1; col <= side; col++) {
-                System.out.print(" . ");
+
+        //printing rows UPPER PART
+        for (int row = 0; row < side; row++) {
+
+            //printing empty space
+            for (int emp = 1; emp < side - row; emp++) {
+                System.out.print(" ");
             }
 
+            //printing stars
+            for (int star = side; star >= side - row; star--) {
+                if (star == side) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" *");
+                }
+            }
             System.out.println();
         }
+
+        //printing rows LOWER PART
+        for (int rowL = 1; rowL <= side - 1; rowL++) {
+
+            //printing empty space
+            for (int empL = side - 1; empL >= side - rowL; empL--) {
+                System.out.print(" ");
+            }
+
+            //printing stars
+            for (int starL = 0; starL < side - rowL; starL++) {
+                if (starL == 0) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" *");
+                }            }
+            System.out.println();
+        }
+
     }
 }
